f021a7a1524208f60eb56fd07f98499cf4923f0a
dhusakshata <130928825+dhusakshata@users.noreply.github.com>
2023-07-05 22:03:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your JS code here. If required.
+<!DOCTYPE html>
+ 
+<html>
+ 
+<head>
+	<link rel="stylesheet" href="styles.css">
+</head>
+ 
+<body>  <div id="sizeInfo"><h1></h1></div>
+	<!-- the entire body must be written by student -->
+    <script type="text/javascript" src="./script.js">
+</script>
+ 
+</body>
+ 
+</html>
+ 
+-----------------
+ 
+JS
+ 
+ 
+---
+//your JS code here. If required.
+  let height=this.innerHeight;
+    let width=this.innerWidth;
+   let div=document.getElementById("sizeInfo");
+    let h=div.firstChild;
+    h.innerText="Width: "+width+" and Height: "+height;  
+window.addEventListener("resize",function(){
+    let height=this.innerHeight;
+    let width=this.innerWidth;
+   let div=document.getElementById("sizeInfo");
+    let h=div.firstChild;
+ 
+    h.innerText="Width: "+width+" and Height: "+height; 
+  
+  })
